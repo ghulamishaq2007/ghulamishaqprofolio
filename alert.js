3d21f32502adc35d88119fe1f683932a1b5f4e1d
Ghulam Ishaq <g.ishaq2007@gmail.com>
2025-06-25 02:52:33 -0600

    Add files via upload

---
+++ alert.js
@@ -0,0 +1,11 @@
+<script>
+  window.onload = function() {
+    Swal.fire({
+      position: "center",
+      icon: "success",
+      title: "Welcome to the my site!",
+      showConfirmButton: false,
+      timer: 1500
+    }) };
+   
+</script>
